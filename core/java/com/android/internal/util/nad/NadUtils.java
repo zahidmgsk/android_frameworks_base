45badec91f84abb0193545fbf506310726e8e766
Roman Birg <romanbirg@gmail.com>
2016-10-23 06:43:28 +0200

    NadUtils: Add isPackageInstalled boolean
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9

--- core/java/com/android/internal/util/nad/NadUtils.java
+++ core/java/com/android/internal/util/nad/NadUtils.java
@@ -18,7 +18,9 @@ package com.android.internal.util.nad;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -77,5 +79,25 @@ public class NadUtils {
     // Check to see if device supports A/B (seamless) system updates
     public static boolean isABdevice(Context context) {
         return SystemProperties.getBoolean("ro.build.ab_update", false);
+    }
+
+    // Check to see if a package is installed
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 }
