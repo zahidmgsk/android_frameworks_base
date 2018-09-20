4b9e610f6eca55a5784535badbca9e7f09a832d4
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-20 15:10:37 +0530

    Statusbar Clock Customization [1/2] Add Clock size, color & font style.
    Port to Pie + Added Color for Clocks - SuperDroidBond
    Clock fonts and size - @temasek
    Port to Oreo by @varund7726

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5818,6 +5818,24 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font color
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_COLOR = "status_bar_clock_color";
+
+        /**
+         * Clock font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_FONT_STYLE = "status_bar_clock_font_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
