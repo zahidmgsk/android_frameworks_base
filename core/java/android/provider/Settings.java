c97e06ad140ddcfeec2f006f69892a4323b13d84
xyyx <xyyx@mail.ru>
2018-09-25 19:57:45 +0000

    base: Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I9b565d258ec060e4d3b7bb4db0d0790ad5e9c037
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6038,6 +6038,12 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide 
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6198,6 +6204,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_OFF_ANIMATION);
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
             PRIVATE_SETTINGS.add(FOD_GESTURE);
+            PRIVATE_SETTINGS.add(FP_UNLOCK_KEYSTORE);
         }
 
         /**
