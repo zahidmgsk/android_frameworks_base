cde12953d1571a4fff0472a864b4aa1948aae3e3
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/com/android/internal/util/nad/NadUtils.java
+++ core/java/com/android/internal/util/nad/NadUtils.java
@@ -32,6 +32,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.util.DisplayMetrics;
 
 import com.android.internal.R;
 
@@ -155,5 +156,23 @@ public class NadUtils {
         if (mUiModeManager == null) return false;
         int mode = mUiModeManager.getNightMode();
         return (mode == UiModeManager.MODE_NIGHT_YES);
+    }
+
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
