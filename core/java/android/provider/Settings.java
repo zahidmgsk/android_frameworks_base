78c250a9f24f063aeed1ed60e70a78870bbba72f
Pranav Vashi <neobuddy89@gmail.com>
2019-02-24 15:34:42 +0530

    Network Traffic: Customize refresh interval [1/2]
    Change-Id: Ica432e43e4eaab2c54aa9154389e8097d14eb6bc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4848,6 +4848,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_LOCATION = "network_traffic_location";
 
         /**
+         * Specify refresh duration for network traffic
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_REFRESH_INTERVAL = "network_traffic_refresh_interval";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
