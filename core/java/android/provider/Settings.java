ec7648a3fd81c892fd34f539f771c0dd76d87ac2
Sam Mortimer <sam@mortimer.me.uk>
2019-08-15 19:40:55 -0700

    fw/b: [Squash] Add capability to allow tethering to use VPN upstreams
    Squash of:
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 15 19:40:55 2019 -0700
    
        fw/b: Add capability to allow tethering to use VPN upstreams
    
        * Toggled on/off at runtime via a new hotspot lineage setting.
    
        * Dynamically updates the tethering upstream for existing hotspot
          clients as VPNs are brought up / down or the hotspot setting
          is changed.
    
        * This implementation depends on fw/b config_tether_upstream_automatic
          being set to true.
    
        Change-Id: I2ac0b4acc0ea686dfdf54561cb3428808e337160
    
    Change-Id: I610565e75a98df81d4f45a83feaba3233217916e
    
    ----
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Sep 13 16:27:37 2019 -0700
    
        fw/b: Prevent trying to enable hw offload for tethering via VPN upstreams
    
        * Tethering via VPN upstream requires a sw path.
    
        * hw offload setup happened to be being disabled anyway owing to a fail
          return code from setDataLimit().  However, it was causing offload to be
          disabled entirely (until next hotspot off / on event).
    
        * Gracefully skip hw offload for vpn upstreams so that it is automatically
          used again when a vpn is disconnected.
    
        Change-Id: I4df13f02889305560903b7b1e919eedc7af78c07
    
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17
    
    Signed-off-by: iKeramat <ikeramat80@gmail.com>
    Change-Id: Ifd565b9455bcbaec87bfd8e759bc43f9bc06052b
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9905,6 +9905,11 @@ public final class Settings {
         public static final int ACCESSIBILITY_MAGNIFICATION_MODE_WINDOW = 0x2;
 
         /**
+         * Whether tethering is allowed to use VPN upstreams
+         */
+        public static final String TETHERING_ALLOW_VPN_UPSTREAMS = "tethering_allow_vpn_upstreams";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
