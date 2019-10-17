048ebacd0efb43cceace144b2e4e2c9328bdaa87
Ido Ben-Hur <idoybh2@gmail.com>
2019-10-17 04:15:53 +0300

    base: Add a toggle for QS footer text [1/2]
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5266,6 +5266,12 @@ public final class Settings {
         public static final String ACCENT_COLOR = "accent_color";
 
         /**
+         * Whether footer #Nusantara Project text shows
+         * @hide
+         */
+        public static final String FOOTER_TEXT_SHOW = "footer_text_show";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
