7ce87ebc183763ce24e680135fda59455419e68f
Alex Cruz <du.alexcruz@gmail.com>
2019-12-16 22:50:12 -0500

    Set QS columns defaults back to AOSP values
    Change-Id: Ib09b519352c1c223cd1bc898cd95c81bbc555183
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -131,7 +131,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
                     Settings.System.QS_COLUMNS_LANDSCAPE, 4,
                     UserHandle.USER_CURRENT);
             mResourceRows = Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.QS_ROWS_LANDSCAPE, 1,
+                        Settings.System.QS_ROWS_LANDSCAPE, 2,
                         UserHandle.USER_CURRENT);
         }
 
