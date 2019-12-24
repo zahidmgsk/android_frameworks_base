a310020eb04d9cb1cfc44af575b5c51676479dbe
Dyneteve <dyneteve@pixelexperience.org>
2019-12-24 11:54:33 +0200

    SystemUI: SystemUIGoogleModule: Switch to EnhancedEstimatesGoogleImpl
    * This also reverts 4d7d15cd269b01ac2f2fb1e101495137d387fbaf
    
    Change-Id: Idb1c44fe9a8022f05dedb1a9c3d9bb99973b4592
    Signed-off-by: saikiran <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleModule.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleModule.java
@@ -1,6 +1,9 @@
 package com.google.android.systemui;
 
+import com.android.systemui.power.EnhancedEstimates;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
+
+import com.google.android.systemui.power.EnhancedEstimatesGoogleImpl;
 
 import dagger.Binds;
 import dagger.Module;
@@ -11,6 +14,10 @@ import dagger.Module;
  */
 @Module
 public abstract class SystemUIGoogleModule {
+
+    @Binds
+    abstract EnhancedEstimates bindEnhancedEstimates(
+        EnhancedEstimatesGoogleImpl enhancedEstimates);
 
     @Binds
     abstract NotificationLockscreenUserManager bindNotificationLockscreenUserManager(
