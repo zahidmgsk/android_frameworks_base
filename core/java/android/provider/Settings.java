e3531cc5372864bd9166ab1423f45fdc06e30700
El Dainosor <eldainosor@gmail.com>
2020-05-17 02:05:38 -0700

    Screen rec: Add support for AudioPlaybackCapture
    Using the new API to record internal audio, and using the MediaMuxer workaround
    because of google only allowing AudioRecord as audio output.
    
    Also, full credits to kcrimi for the MediaMuxer code.
    Taken from recorder-activity from:
    https://github.com/kcrimi/recorder-activity
    
    Bootleggers (ElDainosor) edit: Adapted to AOSP screen recorder code. Added stereo support and skip the pause function
    (as the normal MediaRecorder process has pause support)
    
    Co-authored-by: kcrimi <kevincrimi@gmail.com>
    Change-Id: Icca7314e93b55b04461497bfdbc863c67429cf5c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6493,6 +6493,12 @@ public final class Settings {
         public static final String NUSANTARA_WINGS_STYLE = "nusantara_wings_style";
 
         /**
+         * Screenrecord: audio source
+         * @hide
+         */
+        public static final String SCREENRECORD_AUDIO_SOURCE = "screenrecord_audio_source";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
