Evolution-X-AOSP/frameworks_base

SystemUI: ScreenshotTile: Migrate to new screenshot method with Longshot support
Change-Id: I53e25d8a1f96feedbb6546a76e62a11c615535db
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -15,25 +15,22 @@
 
 package com.android.systemui.qs.tiles;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.UserHandle;
-import android.provider.Settings;
-import android.service.quicksettings.Tile;
+import android.os.RemoteException;
+import android.text.format.DateUtils;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 
-import com.android.internal.util.evolution.EvolutionUtils;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.systemui.qs.QSHost;
-import com.android.systemui.plugins.qs.QSTile.BooleanState;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import javax.inject.Inject;
 
-/** Quick settings tile: Screenshot **/
 public class ScreenshotTile extends QSTileImpl<BooleanState> {
-
-    private boolean mRegion = false;
 
     @Inject
     public ScreenshotTile(QSHost host) {
@@ -41,8 +38,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.EVO_SETTINGS;
+    public void handleSetListening(boolean listening) {
     }
 
     @Override
@@ -51,24 +47,15 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleSetListening(boolean listening) {
-    }
-
-    @Override
-    public void handleClick() {
-        mRegion = !mRegion;
-        refreshState();
+    protected void handleClick() {
+        mHost.collapsePanels();
+        mHandler.postDelayed(() -> takeScreenshot(false), DateUtils.SECOND_IN_MILLIS);
     }
 
     @Override
     public void handleLongClick() {
         mHost.collapsePanels();
-
-        //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        EvolutionUtils.takeScreenshot(mRegion ? false : true);
+        mHandler.postDelayed(() -> takeScreenshot(true), DateUtils.SECOND_IN_MILLIS);
     }
 
     @Override
@@ -83,16 +70,23 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRegion) {
-            state.label = mContext.getString(R.string.quick_settings_region_screenshot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_region_screenshot);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_region_screenshot_label);
-        } else {
-            state.label = mContext.getString(R.string.quick_settings_screenshot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_screenshot_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
+        state.label = mContext.getString(R.string.quick_settings_screenshot_label);
+    }
+
+    private void takeScreenshot(final boolean partial) {
+        final int type = partial
+                ? WindowManager.TAKE_SCREENSHOT_SELECTED_REGION
+                : WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
+        try {
+            WindowManagerGlobal.getWindowManagerService().takeAlternativeScreenshot(type);
+        } catch (RemoteException e) {
+            // Do nothing
         }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVO_SETTINGS;
     }
 }

zahidmgsk/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -15,25 +15,22 @@
 
 package com.android.systemui.qs.tiles;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.UserHandle;
-import android.provider.Settings;
-import android.service.quicksettings.Tile;
+import android.os.RemoteException;
+import android.text.format.DateUtils;
+import android.view.WindowManager;
+import android.view.WindowManagerGlobal;
 
-import com.android.internal.util.du.ActionUtils;
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.systemui.qs.QSHost;
-import com.android.systemui.plugins.qs.QSTile.BooleanState;
-import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.R;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import javax.inject.Inject;
 
-/** Quick settings tile: Screenshot **/
 public class ScreenshotTile extends QSTileImpl<BooleanState> {
-
-    private boolean mRegion = false;
 
     @Inject
     public ScreenshotTile(QSHost host) {
@@ -41,8 +38,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_QUICK_TILES;
+    public void handleSetListening(boolean listening) {
     }
 
     @Override
@@ -51,24 +47,15 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void handleSetListening(boolean listening) {
-    }
-
-    @Override
-    public void handleClick() {
-        mRegion = !mRegion;
-        refreshState();
+    protected void handleClick() {
+        mHost.collapsePanels();
+        mHandler.postDelayed(() -> takeScreenshot(false), DateUtils.SECOND_IN_MILLIS);
     }
 
     @Override
     public void handleLongClick() {
         mHost.collapsePanels();
-
-        //finish collapsing the panel
-        try {
-             Thread.sleep(1000); //1s
-        } catch (InterruptedException ie) {}
-        ActionUtils.takeScreenshot(mRegion ? false : true);
+        mHandler.postDelayed(() -> takeScreenshot(true), DateUtils.SECOND_IN_MILLIS);
     }
 
     @Override
@@ -83,16 +70,23 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRegion) {
-            state.label = mContext.getString(R.string.quick_settings_region_screenshot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_region_screenshot);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_region_screenshot_label);
-        } else {
-            state.label = mContext.getString(R.string.quick_settings_screenshot_label);
-            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
-            state.contentDescription =  mContext.getString(
-                    R.string.quick_settings_screenshot_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
+        state.label = mContext.getString(R.string.quick_settings_screenshot_label);
+    }
+
+    private void takeScreenshot(final boolean partial) {
+        final int type = partial
+                ? WindowManager.TAKE_SCREENSHOT_SELECTED_REGION
+                : WindowManager.TAKE_SCREENSHOT_FULLSCREEN;
+        try {
+            WindowManagerGlobal.getWindowManagerService().takeAlternativeScreenshot(type);
+        } catch (RemoteException e) {
+            // Do nothing
         }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_QUICK_TILES;
     }
 }