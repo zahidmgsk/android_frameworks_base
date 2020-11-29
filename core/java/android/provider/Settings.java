75ebcba3a73d611c7bf53e3d981cecb825dcb9b0
Genkzsz11 <genkzsz11@nusantaradevs.org>
2020-11-29 13:53:54 +0700

    Add nusantara category [1/3]
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5844,6 +5844,11 @@ public final class Settings {
         public static final String QS_TILE_ICON_PRIMARY = "qs_tile_icon_primary";
 
         /**
+         * @hide
+         */
+        public static final String CATEGORY_NUSANTARA = "cat_nusantara";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
