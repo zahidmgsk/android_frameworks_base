b502cebfd54ce4589dc52e06ee0ee4f7ba8a7465
Ali B <abittin@gmail.com>
2021-01-01 20:08:09 +0300

    SystemUI: powermenu: make screenshot optional (1/2)
    Give users the option of removing screenshot from the powermenu.
    Default is screenshot enabled.
    
    Change-Id: I2cd3769a3e755818cb6ca171fd67f7a1b038ccf1
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10348,6 +10348,11 @@ public final class Settings {
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
         /**
+         * @hide
+         */
+        public static final String  SCREENSHOT_IN_POWER_MENU = "screenshot_in_power_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
